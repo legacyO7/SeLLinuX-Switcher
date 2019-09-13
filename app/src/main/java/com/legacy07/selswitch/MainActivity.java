5109b0e2d351b56c2afa755f3bc052f69f4153c5
legacyO7 <you@example.com>
2019-09-13 16:28:56 +0530

    improve ui

--- app/src/main/java/com/legacy07/selswitch/MainActivity.java
+++ app/src/main/java/com/legacy07/selswitch/MainActivity.java
@@ -1,55 +1,82 @@
 package com.legacy07.selswitch;
 
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Switch aSwitch;
-    TextView tv;
+    Typeface typeface, typeface1;
+    AssetManager am, am1;
+    TextView tv, banner, title, ii;
+    Animation aniFade, aniBlink, aniZoomin, aniZoomout, aniMove, aniSlup, aniSldn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
+        title = findViewById(R.id.textView);
         aSwitch = findViewById(R.id.selswitch);
+        banner = findViewById(R.id.legacy);
 
-        if (runcommand("su -c 'getenforce'").equals(""))
-        {
+        am = getApplicationContext().getAssets();
+        typeface = Typeface.createFromAsset(am, String.format(Locale.US, "fonts/%s", "GreatVibes-Regular.otf"));
+        am1 = getApplicationContext().getAssets();
+        typeface1 = Typeface.createFromAsset(am1, String.format(Locale.US, "fonts/%s", "Pacifico.ttf"));
+
+
+        title.setTypeface(typeface);
+        aSwitch.setTypeface(typeface);
+
+        tv.setTypeface(typeface1);
+        //banner.setTypeface(typeface1);
+
+
+        aniFade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        aniBlink = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        banner.startAnimation(aniBlink);
+
+        if (runcommand("su -c 'getenforce'").equals("")) {
             tv.setText("No Root Detected");
             tv.setTextSize(30);
             aSwitch.setVisibility(View.INVISIBLE);
-        }
+        } else {
 
+            if (runcommand("su -c 'getenforce'").contains("Enforcing")) {
+                aSwitch.setChecked(true);
+                tv.setText("Status : Enforcing");
+            } else tv.setText("Status : Permissive");
 
-        if (runcommand("su -c 'getenforce'").contains("Enforcing")) {
-            aSwitch.setChecked(true);
-        }
+            aSwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
+                    tv.startAnimation(aniFade);
 
-
-        aSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (runcommand("su -c 'getenforce'").contains("Enforcing")) {
-                    runcommand("su -c " + '"' + '"' + "setenforce 0" + '"' + '"');
-                    aSwitch.setChecked(false);
-                } else {
-                    runcommand("su -c " + '"' + '"' + "setenforce 1" + '"' + '"');
-                    aSwitch.setChecked(true);
+                    if (runcommand("su -c 'getenforce'").contains("Enforcing")) {
+                        runcommand("su -c " + '"' + '"' + "setenforce 0" + '"' + '"');
+                        aSwitch.setChecked(false);
+                    } else {
+                        runcommand("su -c " + '"' + '"' + "setenforce 1" + '"' + '"');
+                        aSwitch.setChecked(true);
+                    }
+                    tv.setText("Status : " + runcommand("su -c 'getenforce'"));
                 }
-                tv.setText("Changed to " + runcommand("su -c 'getenforce'"));
-            }
-        });
+            });
+        }
 
     }
 
@@ -72,4 +99,27 @@ public class MainActivity extends AppCompatActivity {
         }
         return log.toString();
     }
+
 }
+
+/*
+repo init -u https://gitlab.com/OrangeFox/Manifest.git -b fox_8.1
+repo sync -j8 --force-sync
+git clone https://github.com/rk779/twrp_device_asus_X01BD.git device/asus/X01BD
+cd device/asus/X01BD
+echo add_lunch_combo omni_X01BD-eng>vendorsetup.sh
+chmod +x vendorsetup.sh
+cd ../../..
+ . build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_FLASHLIGHT_ENABLE="0"
+export FOX_NOTCH="1"
+export FOX_RESET_SETTINGS="1"
+export OF_MAINTAINER="legacy07"
+export BUILD_TYPE=Stable
+export TW_DEVICE_VERSION="R10.0-5"
+lunch omni_X01BD-eng
+mka recoveryimage
+
+ */
